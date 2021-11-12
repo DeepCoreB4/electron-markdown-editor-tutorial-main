47d883c54b5a94a10a85e8b8731c1777bea72757
cuparikun <cupparikun@gmail.com>
2021-11-12 11:09:00 +0100

    ....

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -1,3 +1,10 @@
 import {useElectron} from '/@/use/electron' html, body { background-color:
 transparent; height: 100%; } style=" background: transparent;" const
 {readConfig} = useElectron()
+
+const App = new BrowserWindow({
+    transparent: true,
+    frame: false,
+    fullscreen: true,
+})
+mainWindow.setIgnoreMouseEvents(true)
--- packages/renderer/src/app.tsx
+++ packages/renderer/src/app.tsx
@@ -4,15 +4,16 @@ import Preview from './preview'
 import './app.css'
 
 const App: React.FC = () => {
-  
-  const [doc, setDoc] = useState<string>('# Hello, Jutzu! \n')
+  const [doc, setDoc] = useState<string>(
+    '# Hello, Jutzu!\n```javascript \nvar s = "DC-Fox Markdown Editor by Edgar Cuppari"; \n alert(s);\n``` \n![alt text][logo] \n \n[logo]:https://raw.githubusercontent.com/DeepCoreB4/electron-markdown-editor-tutorial-main/master/buildResources/icon.png "Logo Title Text 2"\n '
+  )
 
   const handleDocChange = useCallback(newDoc => {
     setDoc(newDoc)
   }, [])
 
   return (
-    <div className='app'>
+    <div className="app">
       <Editor onChange={handleDocChange} initialDoc={doc} />
       <Preview doc={doc} />
     </div>
